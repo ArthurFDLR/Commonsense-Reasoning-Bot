0900026c86583512e22cd90bd1ed0e2da4c3c181
Arthur FINDELAIR <arthfind@gmail.com>
2020-08-04 16:02:22 +0200

    Training data size comparison

--- AppHandClassifier/PoseClassifier.py
+++ AppHandClassifier/PoseClassifier.py
@@ -20,7 +20,7 @@ def showModelComparison(modelsDict, sizePlot, commonDescription):
     '''
 
     fig, axes = plt.subplots(sizePlot[0], sizePlot[1])
-    plt.text(x=0.5, y=0.94, s='MNIST dataset, Epochs size: ' + str(sizeTrainingSet) + ' (' + str((sizeTrainingSet*100)//sizeTrainingSetInit) + '%)', fontsize=20, ha="center", transform=fig.transFigure)
+    plt.text(x=0.5, y=0.94, s='Title', fontsize=20, ha="center", transform=fig.transFigure)
     plt.text(x=0.5, y=0.88, s= commonDescription, fontsize=17, ha="center", transform=fig.transFigure)
 
 
@@ -34,25 +34,55 @@ def showModelComparison(modelsDict, sizePlot, commonDescription):
         for counter, (name, values) in enumerate(history.items()):
             ax.plot(values, label=name, marker='o', color='blue' if counter%2==0 else 'red', linestyle='--' if counter<2 else '-')
             if counter == 3:
-                ax.set_title(nameModel + '\nBest accuracy (epochs=' + str(np.argmax(values)+1) + '): ' + str(round(max(values)*100.0,4)) + '%')
-        ax.set_xticks([i for i in range(nbrEpochsMax)])
+                ax.set_title(nameModel + '\nBest accuracy ' + '): ' + str(round(max(values)*100.0,4)) + '%')
+        #ax.set_xticks([i for i in range(nbrEpochsMax)])
         ax.legend()
     plt.subplots_adjust(top=0.8, wspace=0.2, right=0.95, left=0.05, bottom=0.05, hspace=0.35)
     plt.show()
 
-def Comparison_TrainingSize(classOutput:list, handID:int):
-    modelsDict = {}
+def showTrainingSizeComparison(modelsDict, commonDescription, nbrEpochsMax:int=-1):
+    '''Display the evolution of Loss and Accuracy versus epochs given histories of training
 
-    allSamples_x, allSamples_y_oneHot = loadDataset(classOutput, -1, handID,True)
+    Parameters:
+        modelsDict (Dict{ str:Dict{ str:list[float] } }): Dictionary containing the training history of a model referenced by its name
+        commonDescription (str): Common caracteristics of the models
+        nbrEpochsMax (int): Epochs of training, use to set x_axis ticks. Use standard ticks if negative.
+    
+    Return:
+        Matplotlib figure: Comparison of given models
+    '''
+
+    resultsCategories = ['loss', 'accuracy', 'val_loss', 'val_accuracy']
+    fig, axes = plt.subplots(2, 2)
+    plt.text(x=0.5, y=0.94, s='Training dataset size comparison', fontsize=20, ha="center", transform=fig.transFigure)
+    plt.text(x=0.5, y=0.88, s= commonDescription, fontsize=17, ha="center", transform=fig.transFigure)
+
+    for i in range(4):
+        try:
+            ax = axes.flatten()[i]
+        except:
+            ax=axes
+        
+        ax.set_title(resultsCategories[i])
+
+        if i%2 == 1:
+            ax.set_ylim(0.9,1.05)
+        if nbrEpochsMax>0:
+            ax.set_xticks([i+1 for i in range(nbrEpochsMax)])
+
+        for nameModel, history in modelsDict.items():
+            ax.plot([i+1 for i in range(nbrEpochsMax)], history[resultsCategories[i]], label=nameModel, marker='o') #color='blue' if i%2==0 else 'red', linestyle='--' if i<2 else '-'
+        ax.legend()
+    plt.subplots_adjust(top=0.8, wspace=0.2, right=0.95, left=0.05, bottom=0.05, hspace=0.35)
+    plt.show()
+
+def Comparison_TrainingSize(classOutput:list, handID:int, epochs:int):
+    modelsDict = {}
 
     for size in [25, 50, 75, 100, 125, 150, 175, 200]:
 
-        trainSamples_x = allSamples_x[:size]
-        trainSamples_y = allSamples_y_oneHot[:size]
-        testSamples_x  = allSamples_x[size:]
-        testSamples_y  = allSamples_y_oneHot[size:]
-
-
+        trainSamples_x, trainSamples_y, testSamples_x, testSamples_y = loadDataset(classOutput, size, handID,True)
+        
         model = tf.keras.models.Sequential()
         model.add(tf.keras.layers.Dense(32, input_dim=42, activation=tf.keras.activations.relu))
         model.add(tf.keras.layers.Dense(32, activation=tf.keras.activations.relu))
@@ -63,7 +93,7 @@ def Comparison_TrainingSize(classOutput:list, handID:int):
                     loss='categorical_crossentropy', # prefere loss='sparse_categorical_crossentropy' if not one-hot encoded
                     metrics=['accuracy'])
 
-        hist = model.fit(x=trainSamples_x, y=trainSamples_y, epochs=9, batch_size=20,  validation_data=(testSamples_x,testSamples_y)).history
+        hist = model.fit(x=trainSamples_x, y=trainSamples_y, epochs=epochs, batch_size=20,  validation_data=(testSamples_x,testSamples_y)).history
 
         modelsDict['Size:' + str(size)] = hist
     
@@ -129,44 +159,58 @@ def loadDataset(classOutput:list, samplePerClass:int, handID:int, onehotEncoding
     trainSamples_x = []
     testSamples_x = []
     trainSamples_y = []
-    trainSamples_y_oneHot = []
+    testSamples_y = []
     for i, className in enumerate(classOutput):
         loadedSampels = loadFile(className, handID)
         if samplePerClass > 0:
             trainingLoad = loadedSampels[:min(samplePerClass, len(loadedSampels)-1)]
             testingLoad = loadedSampels[min(samplePerClass, len(loadedSampels)-1):]
+        else:
+            trainingLoad = loadedSampels
+            testingLoad = []
+
         trainSamples_x += trainingLoad
         testSamples_x += testingLoad
-        trainSamples_y += [i for j in range(len(trainingLoad))]
-
         outPut_tmp = [0]*len(classOutput)
         outPut_tmp[i] = 1
-        trainSamples_y_oneHot += [outPut_tmp for j in range(len(trainingLoad))]
+        trainSamples_y += [outPut_tmp for j in range(len(trainingLoad))] if onehotEncoding else [i for j in range(len(trainingLoad))]
+        testSamples_y += [outPut_tmp for j in range(len(testingLoad))] if onehotEncoding else [i for j in range(len(testingLoad))]
     
     trainSamples_x = np.array(trainSamples_x)
     testSamples_x = np.array(testSamples_x)
     trainSamples_y = np.array(trainSamples_y)
-    trainSamples_y_oneHot = np.array(trainSamples_y_oneHot)
+    testSamples_y = np.array(testSamples_y)
 
     index = np.arange(trainSamples_x.shape[0])
     np.random.shuffle(index)
     trainSamples_x = trainSamples_x[index]
-    testSamples_x = testSamples_x[index]
     trainSamples_y = trainSamples_y[index]
-    trainSamples_y_oneHot = trainSamples_y_oneHot[index]
+    
+    index = np.arange(testSamples_x.shape[0])
+    np.random.shuffle(index)
+    testSamples_x = testSamples_x[index]
+    testSamples_y = testSamples_y[index]
     
     if len(trainSamples_x) < samplePerClass*len(classOutput):
         print('Size expectation not met.')
 
-    return trainSamples_x, (trainSamples_y_oneHot if onehotEncoding else trainSamples_y)
+    return trainSamples_x, trainSamples_y, testSamples_x, testSamples_y
 
 if __name__ == "__main__":
 
     IdTesting, IdModelSaving, IdModelComparison = range(3)
-    SELECTOR = 1
+    SELECTOR = 2
 
     if SELECTOR == IdModelComparison:
-        pass
+        classFingerCount = ['1_Eng', '2_Eng', '3_Eng', '4_Eng', '5']
+        classRestaurant = ['Chef', 'Help', 'Super', 'VIP', 'Water']
+        classDivers = ['Metal']
+        classOutput = classFingerCount + classRestaurant + classDivers
+        epochsNbr = 10
+        dictResults = Comparison_TrainingSize(classOutput,0,epochsNbr)
+        #print(dictResults)
+        showTrainingSizeComparison(dictResults, '2 dense hidden layers of 32 neurons (Rectified linear activation) - Adam optimizer - Cross entropy loss - ' + str(len(classOutput)) + 'outputs categories (Softmax activation)', epochsNbr)
+
 
     if SELECTOR == IdModelSaving:
 
@@ -176,8 +220,10 @@ if __name__ == "__main__":
         classOutput = classFingerCount + classRestaurant + classDivers
         handID = 0
 
-        allSamples_x, allSamples_y_oneHot = loadDataset(classOutput, -1, handID,True)
+        allSamples_x, allSamples_y_oneHot, _, _ = loadDataset(classOutput, 100, handID,True)
         
+        print(len(allSamples_x))
+        print(len(allSamples_y_oneHot))
         ## Model definition
 
         model = tf.keras.models.Sequential()
