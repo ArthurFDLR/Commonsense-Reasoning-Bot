90f8fbc022cc3b6789626088f89aa986ce6f61bf
Arthur FINDELAIR <arthfind@gmail.com>
2020-08-06 16:36:59 +0200

    Improve classifier model

--- AppHandClassifier/PoseClassifier.py
+++ AppHandClassifier/PoseClassifier.py
@@ -7,6 +7,9 @@ import math
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
+#plt.xkcd()
+plt.style.use('ggplot')
+
 def showModelComparison(modelsDict, sizePlot, commonDescription):
     '''Display the evolution of Loss and Accuracy versus epochs given histories of training
 
@@ -40,12 +43,13 @@ def showModelComparison(modelsDict, sizePlot, commonDescription):
     plt.subplots_adjust(top=0.8, wspace=0.2, right=0.95, left=0.05, bottom=0.05, hspace=0.35)
     plt.show()
 
-def showTrainingSizeComparison(modelsDict, commonDescription, nbrEpochsMax:int=-1):
+def showTrainingSizeComparison(modelsDict, fileName:str, commonDescription:str, nbrEpochsMax:int=-1):
     '''Display the evolution of Loss and Accuracy versus epochs given histories of training
 
     Parameters:
         modelsDict (Dict{ str:Dict{ str:list[float] } }): Dictionary containing the training history of a model referenced by its name
-        commonDescription (str): Common caracteristics of the models
+        fileName (str): Name of the pgn file generated.
+        commonDescription (str): Common caracteristics of the models.
         nbrEpochsMax (int): Epochs of training, use to set x_axis ticks. Use standard ticks if negative.
     
     Return:
@@ -53,6 +57,7 @@ def showTrainingSizeComparison(modelsDict, commonDescription, nbrEpochsMax:int=-
     '''
 
     resultsCategories = ['loss', 'accuracy', 'val_loss', 'val_accuracy']
+    nameCategories = ['Training loss', 'Training accuracy', 'Validation loss', 'Validation accuracy']
     fig, axes = plt.subplots(2, 2)
     plt.text(x=0.5, y=0.94, s='Training dataset size comparison', fontsize=20, ha="center", transform=fig.transFigure)
     plt.text(x=0.5, y=0.88, s= commonDescription, fontsize=17, ha="center", transform=fig.transFigure)
@@ -63,30 +68,39 @@ def showTrainingSizeComparison(modelsDict, commonDescription, nbrEpochsMax:int=-
         except:
             ax=axes
         
-        ax.set_title(resultsCategories[i])
+        ax.set_title(nameCategories[i])
 
-        if i%2 == 1:
-            ax.set_ylim(0.9,1.05)
         if nbrEpochsMax>0:
             ax.set_xticks([i+1 for i in range(nbrEpochsMax)])
 
         for nameModel, history in modelsDict.items():
-            ax.plot([i+1 for i in range(nbrEpochsMax)], history[resultsCategories[i]], label=nameModel, marker='o') #color='blue' if i%2==0 else 'red', linestyle='--' if i<2 else '-'
+            ax.plot([i+1 for i in range(nbrEpochsMax)], history[resultsCategories[i]], label=nameModel) #color='blue' if i%2==0 else 'red', linestyle='--' if i<2 else '-'
         ax.legend()
+        #ax.grid()
     plt.subplots_adjust(top=0.8, wspace=0.2, right=0.95, left=0.05, bottom=0.05, hspace=0.35)
-    plt.show()
 
-def Comparison_TrainingSize(classOutput:list, handID:int, epochs:int):
+    fig1 = plt.gcf()
+    fig1.set_size_inches((20, 12), forward=False)
+    fig1.savefig('.\\ModelsComparisons\\' + fileName + '.png', dpi=300)
+
+    axes.flatten()[0].set_ylim(0.0,0.25)
+    axes.flatten()[1].set_ylim(0.95,1.02)
+    axes.flatten()[2].set_ylim(0.0,0.25)
+    axes.flatten()[3].set_ylim(0.95,1.02)
+    fig2 = plt.gcf()
+    fig2.set_size_inches((20, 12), forward=False)
+    fig2.savefig('.\\ModelsComparisons\\' + fileName + '_Zoomed.png', dpi=300)
+
+    #plt.show()
+
+def Comparison_TrainingSize(model:tf.keras.models ,classOutput:list, handID:int, epochs:int):
+    model.save_weights('.\\Data\\modelTmp.h5')
     modelsDict = {}
 
-    for size in [25, 50, 75, 100, 125, 150, 175, 200]:
+    for size in [25, 50, 75, 100, 125, 150, 175]:
+        model.load_weights('.\\Data\\modelTmp.h5')
 
         trainSamples_x, trainSamples_y, testSamples_x, testSamples_y = loadDataset(classOutput, size, handID,True)
-        
-        model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.Dense(32, input_dim=42, activation=tf.keras.activations.relu))
-        model.add(tf.keras.layers.Dense(32, activation=tf.keras.activations.relu))
-        model.add(tf.keras.layers.Dense(len(classOutput), activation=tf.keras.activations.softmax))
 
         model.summary()
         model.compile(optimizer=tf.keras.optimizers.Adam(),
@@ -95,11 +109,11 @@ def Comparison_TrainingSize(classOutput:list, handID:int, epochs:int):
 
         hist = model.fit(x=trainSamples_x, y=trainSamples_y, epochs=epochs, batch_size=20,  validation_data=(testSamples_x,testSamples_y)).history
 
-        modelsDict['Size:' + str(size)] = hist
+        modelsDict[str(size) + ' samples per pose'] = hist
     
     return modelsDict
 
-def loadFile(poseName:str, handID:int):
+def loadFile(poseName:str, handID:int, shuffle:bool=True):
     fileName = '.\\Datasets\\{}\\{}\\data.txt'.format(poseName, 'right_hand' if handID == 1 else 'left_hand')
     listOut = []
     if os.path.exists(fileName):
@@ -134,9 +148,15 @@ def loadFile(poseName:str, handID:int):
                     for i in range(len(currentEntry[0])):
                         listOut[-1].append(currentEntry[0][i])
                         listOut[-1].append(currentEntry[1][i])
-
         dataFile.close()
-
+        
+        listOut = np.array(listOut)
+        if shuffle:
+            index = np.arange(listOut.shape[0])
+            np.random.shuffle(index)
+            listOut = listOut[index]
+    else:
+        listOut = np.array(listOut)
     return listOut
 
 def saveModel(model:tf.keras.models, name:str, handID:int, outputClass):
@@ -161,7 +181,7 @@ def loadDataset(classOutput:list, samplePerClass:int, handID:int, onehotEncoding
     trainSamples_y = []
     testSamples_y = []
     for i, className in enumerate(classOutput):
-        loadedSampels = loadFile(className, handID)
+        loadedSampels = loadFile(className, handID, True).tolist()
         if samplePerClass > 0:
             trainingLoad = loadedSampels[:min(samplePerClass, len(loadedSampels)-1)]
             testingLoad = loadedSampels[min(samplePerClass, len(loadedSampels)-1):]
@@ -198,34 +218,25 @@ def loadDataset(classOutput:list, samplePerClass:int, handID:int, onehotEncoding
 
 if __name__ == "__main__":
 
-    IdTesting, IdModelSaving, IdModelComparison = range(3)
-    SELECTOR = 1
+    IdModelSaving, IdModelComparison = range(2)
+    SELECTOR = 0
 
     classFingerCount = ['0', '1_Eng', '2_Eng', '2_Eu', '3_Eng', '3_Eu', '4', '5']
     classRestaurant = ['Chef', 'Help', 'Super', 'VIP', 'Water']
     classDivers = ['Metal', 'Dislike', 'Loser', 'Phone', 'Shaka', 'Stop', 'Spoke', 'PowerFist', 'Horns', 'FightFist', 'MiddleFinger']
     classOutput = classFingerCount + classRestaurant + classDivers
 
-    if SELECTOR == IdModelComparison:
-        epochsNbr = 10
-        dictResults = Comparison_TrainingSize(classOutput,0,epochsNbr)
-        #print(dictResults)
-        showTrainingSizeComparison(dictResults, '2 dense hidden layers of 32 neurons (Rectified linear activation) - Adam optimizer - Cross entropy loss - ' + str(len(classOutput)) + 'outputs categories (Softmax activation)', epochsNbr)
-
-
     if SELECTOR == IdModelSaving:
 
         for handID in [0,1]:
 
             allSamples_x, allSamples_y_oneHot, _, _ = loadDataset(classOutput, 200, handID,True)
-            
-            print(len(allSamples_x))
-            print(len(allSamples_y_oneHot))
+
             ## Model definition
 
             model = tf.keras.models.Sequential()
-            model.add(tf.keras.layers.Dense(32, input_dim=42, activation=tf.keras.activations.relu))
-            model.add(tf.keras.layers.Dense(32, activation=tf.keras.activations.relu))
+            model.add(tf.keras.layers.Dense(128, input_dim=42, activation=tf.keras.activations.relu))
+            model.add(tf.keras.layers.Dense(128, activation=tf.keras.activations.relu))
             model.add(tf.keras.layers.Dense(len(classOutput), activation=tf.keras.activations.softmax))
 
             model.summary()
@@ -233,6 +244,44 @@ if __name__ == "__main__":
                         loss='categorical_crossentropy', # prefere loss='sparse_categorical_crossentropy' if not one-hot encoded
                         metrics=['accuracy'])
 
-            hist = model.fit(x=allSamples_x, y=allSamples_y_oneHot, epochs=10, batch_size=20,  validation_split=0.15).history
+            hist = model.fit(x=allSamples_x, y=allSamples_y_oneHot, epochs=17, batch_size=20, validation_split=0.0).history
 
-            saveModel(model, 'FullDataset', handID, classOutput)
+            saveModel(model, '24Output-2x128-17epochs', handID, classOutput)
+    
+
+    if SELECTOR == IdModelComparison:
+        maxEpochsNbr = 30
+
+
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(64, input_dim=42, activation=tf.keras.activations.relu))
+        model.add(tf.keras.layers.Dense(64, activation=tf.keras.activations.relu))
+
+        model.add(tf.keras.layers.Dense(len(classOutput), activation=tf.keras.activations.softmax))
+
+        dictResults = Comparison_TrainingSize(model, classOutput, 0, maxEpochsNbr)
+        showTrainingSizeComparison(dictResults, '2x64_Adam_Relu',
+                                   '2 dense hidden layers of 64 neurons (Rectified linear activation) - Adam optimizer - Cross entropy loss - ' + str(len(classOutput)) + ' outputs categories (Softmax activation)',
+                                   maxEpochsNbr)
+        '''
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(32, input_dim=42, activation=tf.keras.activations.relu))
+        model.add(tf.keras.layers.Dense(32, activation=tf.keras.activations.relu))
+        model.add(tf.keras.layers.Dense(len(classOutput), activation=tf.keras.activations.softmax))
+
+        dictResults = Comparison_TrainingSize(model, classOutput, 0, maxEpochsNbr)
+        showTrainingSizeComparison(dictResults, '2x32_Adam_Relu',
+                                   '2 dense hidden layers of 32 neurons (Rectified linear activation) - Adam optimizer - Cross entropy loss - ' + str(len(classOutput)) + ' outputs categories (Softmax activation)',
+                                   maxEpochsNbr)
+        
+
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Dense(128, input_dim=42, activation=tf.keras.activations.relu))
+        model.add(tf.keras.layers.Dense(128, activation=tf.keras.activations.relu))
+        model.add(tf.keras.layers.Dense(len(classOutput), activation=tf.keras.activations.softmax))
+
+        dictResults = Comparison_TrainingSize(model, classOutput, 0, maxEpochsNbr)
+        showTrainingSizeComparison(dictResults, '2x128_Adam_Relu',
+                                   '2 dense hidden layers of 128 neurons (Rectified linear activation) - Adam optimizer - Cross entropy loss - ' + str(len(classOutput)) + ' outputs categories (Softmax activation)',
+                                   maxEpochsNbr)
+        '''
